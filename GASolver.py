d0335c2f1794444c6f2b4c259a912e05ca538fd9
Ivan <isvogor@foi.hr>
2015-10-14 20:02:39 +0200

    Refactoring in progress...

---
+++ GASolver.py
@@ -0,0 +1,67 @@
+__author__ = 'Ivan'
+
+import array
+import random
+
+import numpy
+
+from deap import algorithms
+from deap import base
+from deap import creator
+from deap import tools
+from datetime import *
+import Solver
+
+
+class GASolver(Solver):
+
+        # discrete values
+    def solve(self):
+        creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+        creator.create("Individual", array.array, typecode='b', fitness=creator.FitnessMin)
+
+        toolbox = base.Toolbox()
+
+        # Attribute generator
+        toolbox.register("attr_bool", random.randint, 0, 3)
+
+        # Structure initializers
+        toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_bool, 11)
+        toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+        toolbox.register("evaluate", self.evaluate)
+        toolbox.register("mate", tools.cxTwoPoint)
+        toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+        toolbox.register("select", tools.selTournament, tournsize=3)
+
+        ###########################
+
+        random.seed(64)
+
+        pop = toolbox.population(n=300)
+        hof = tools.HallOfFame(1)
+        stats = tools.Statistics(lambda ind: ind.fitness.values)
+        stats.register("avg", numpy.mean)
+        stats.register("std", numpy.std)
+        stats.register("min", numpy.min)
+        stats.register("max", numpy.max)
+
+        startTime = datetime.now()
+        pop, log = algorithms.eaSimple(pop, toolbox, cxpb=0.5, mutpb=0.2, ngen=40,stats=stats, halloffame=hof, verbose=False)
+        endTime = datetime.now()
+
+        best_ind = tools.selBest(pop, 1)[0]
+
+        res = {"result": best_ind,
+               "score": self.fitness_function(best_ind.fitness.values, False),
+               "method": "Genetic Algorithm", "pop" : pop, "log" : log, "hof" : hof,
+               "type": "-", "time":endTime - startTime}
+
+        #print("Best individual is %s, %s" % (best_ind, best_ind.fitness.values))
+
+        return res
+
+    # end method solve_by_genetic_algorithm
+
+    def evaluate(self, individual):
+        return self.fitness_function(individual)
--- main.py
+++ main.py
@@ -2,9 +2,8 @@ __author__ = 'ivan'
 
 import PlatformInitializer as pinit
 import Calculator as calculator
-import Solver as slv
-import SASolver as saslv
-import random
+import Solver as so
+
 
 if __name__ == '__main__':
 
@@ -31,59 +30,8 @@ if __name__ == '__main__':
 
     #calculate the eignenvector
     vec_trade_off_f = calc.eigenvector(initializer.pairwise_matrix)
+    solver = so.Solver()
+    solver.solution_verifier.set_matrices(nComponents, nUnits, vec_trade_off_f, mat_norm_components, mat_norm_resources, mat_norm_units, initializer.resource_matrix, mat_resource_availability, initializer.platform_matrix)
+    solver.solution_verifier.set_architectural_constraints(initializer.preference_matrix, initializer.mandatory_matrix, initializer.forbidden_matrix, initializer.synergy_matrix)
 
-    solver = slv.Solver(nComponents, nUnits, vec_trade_off_f, mat_norm_components, mat_norm_resources, mat_norm_units, initializer.resource_matrix, mat_resource_availability, initializer.platform_matrix)
-    solver.set_architectural_constraints(initializer.preference_matrix, initializer.mandatory_matrix, initializer.forbidden_matrix, initializer.synergy_matrix)
-
-    #results = []
-    #for i in range(0, 1):
-    #    result = solver.solve_by_genetic_algorithm()
-    #    results.append([result["result"], result["time"]])
-    #    print("- %s -", i)
-    #result = solver.solve_by_genetic_algorithm(True, True)
-
-    #f = open('results_deap.txt','w')
-    #for r in results:
-    #    string = str(r[0]) + ", " + str(r[1]) + ", " + str(result["score"][0]) + "\n"
-    #    f.write(string) # python will convert \n to os.linesep
-    #f.close()
-
-    initial_state = [0] * 11
-    slver = saslv.SASolver(initial_state)
-    slver.setup_solver(solver)
-
-    print(initial_state)
-
-    something1, something2 = slver.anneal()
-
-    print(something1, ", ", something2)
-
-    #solver.manual_fitness(something1)
-
-    #solver.manual_fitness(result["result"])
-
-    #mock_solution_good = [0, 2, 2, 0, 0, 0, 3, 1, 1, 2, 1]
-    #mock_solution_bad = [3, 2, 2, 0, 0, 0, 3, 1, 1, 1, 1]
-    #
-    #solver.manual_fitness(result["result"])
-    #
-    ## print result
-    #print ("****************** R E S U L T ********************")
-    #print ("Method: ", result["method"])
-    #print ("Result: ", result["result"])
-    #print ("Score: ", result["score"][0], ", communication ", result["score"][1], ", res ", result["score"][2])
-    #print ("Skipping all on one: ", result["type"])
-    #print ("Runtime: ", result["time"])
-    #print ("Solution space: ", nUnits, "^", nComponents, " = ", pow(nUnits, nComponents))
-    #print ("----------- VALIDITY ----------")
-    #print ("Solution valid: ", solver.is_solution_valid(result["result"]))
-    #print ("Architectural validity\n", solver.is_architectural_valid(result["result"], True))
-    #print ("Mandatory - forbidden consistency: ", solver.mandatory_forbidden_consistency_check())
-    #print ("***************************************************")
-
-    #1.15539650222
-    #2.13983685843
-    #0.29119198373
-    #****************** R E S U L T ********************
-    #Method:  Genetic Algorithm
-    #Result:  array('b', [0, 1, 0, 0, 1, 0, 3, 1, 1, 2, 1])
+    res = solver.solve("GASolver")
